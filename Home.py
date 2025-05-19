7ce6b78e3988428f8c94b7cb07e849565cec4209
mfjgroup <IT@mayfairjets.com>
2025-05-19 15:39:35 +0400

    changed

--- Home.py
+++ Home.py
@@ -1,28 +1,9 @@
 import streamlit as st
-
 import bcrypt
 from datetime import datetime
-
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from utils import get_image_path
-
 st.set_page_config(page_title='Revenue Management System', page_icon="✈", layout="wide")
 #hide_sidebar = False
 st.hide_sidebar=True
-
-import streamlit.components.v1 as components
-import pandas as pd
-import os
-from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import streamlit_authenticator as stauth
- 
-st.set_page_config(page_title='Revenue Management System', page_icon="✈", layout="wide", initial_sidebar_state="collapsed")
-
 #st.sidebar.image("images/logo.jpg",caption="")
 html_title = """
     <html>
@@ -37,11 +18,7 @@ html_title = """
         }
     </style>
      <div class="container">
-
-     <center><h1 class="title-test"> 🛩 MFJ Revenue Management System📊 </h1></center>
-=======
      <center><h1 class="title-test"> 🛩 Revenue Management System 📊 </h1></center>
->>>>>>> 6790240 (changed)
      </div>
      </html>
 """
@@ -102,35 +79,6 @@ def login():
         now = datetime.now()
         date_now = now.strftime("%d-%b-%y %H:%M:%S")
         st.markdown('<span style="color:#800080;font-weight:bold">Time is ' + date_now + '</span>', unsafe_allow_html=True)
-
-        
-        st.markdown(
-    """
-    <html>
-    <body>
-    <style>
-    .time-container {
-        color:#FFFFFF;
-        font-weight:bold; 
-        padding:5px;
-        border-radius:6px
-    }
-    </style>
-    </html>
-    """,
-    unsafe_allow_html=True
-)
-
-        now=datetime.now()
-        date_now=now.strftime("%d-%b-%y %H:%M:%S")
-#
-        st.markdown('<span style="color:#800080;font-weight:bold">Time is '+ date_now +  '</span>', unsafe_allow_html=True)
-    #st.success(f"File saved at {file_path}")
-    #st.write(f"File Name: {uploaded_file.name}")
-    #<li><a href="Home" class="active">Home</a></li>
-
-        st.markdown("")
-
         html_content = """
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -176,14 +124,6 @@ def login():
       <li class="w3-large"><a href="Homet"><i class="fa fa-homet"></i> Revenue & Inventory Report</a></li>
       <li class="w3-large"><a href="PRL"><i class="fa fa-homet"></i> Flown & No-Show Data</a></li>
       <li class="w3-large"><a href="Statistic"><i class="fa fa-homet"></i>Booking Statistics</a></li>
-      <li class="w3-large"><a href="Home"><i class="fa fa-home"></i> Home</a></li>
-      <li class="w3-large"><a href="Inventory">Inventory</a></li>
-      <li class="w3-large"><a href="Revenue">Revenue</a></li>
-      <li class="w3-large"><a href="Transaction">Transaction</a></li>
-      <li class="w3-large"><a href="Payment">Payment</a></li>
-      <li class="w3-large"><a href="Report">MM_DD</a></li>
-      <li class="w3-large"><a href="DaysPrior">Days Prior</a></li>
-      <li class="w3-large"><a href="BookingTrend">Booking Trend Prior</a></li>
       </div>
     </ul>
   </body>
@@ -191,11 +131,6 @@ def login():
 """
  #<li class="w3-large"><a href="MyPivot">MyPivot</a></li>
 # Render the HTML in Streamlit
-        st.markdown(html_content, unsafe_allow_html=True)
-
-        image_path = get_image_path("mayfairjets1.jpg")
-
-# Custom CSS to set the background and make the image take half the height
         st.markdown(html_content, unsafe_allow_html=True)
 
         image_path = "images/mayfairjets1.jpg"
@@ -251,9 +186,3 @@ def login():
                 st.error("Username is incorrect")
 
 login()
-
-
-# Create a container to display the image
- #st.markdown('<div class="image-container">', unsafe_allow_html=True)
- #st.image(image_path)
- #st.markdown('</div>', unsafe_allow_html=True)
