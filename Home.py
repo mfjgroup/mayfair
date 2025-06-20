b44bc23318fc210829595f9e8241919061d3c053
mfjgroup <IT@mayfairjets.com>
2025-06-20 14:01:48 +0400

    new

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
 from streamlit_authenticator.utilities.hasher import Hasher
-pw = ["MFJ2025@rms123456"]
-hashed = Hasher.hash_list(pw)
-print(hashed[0])
+
+# Create a Hasher instance with a list of passwords
+hasher = Hasher(["MFJ2025@rms123456"])
+
+# Call hash_list on the instance
+hashed_passwords = hasher.hash_list()
+
+# Print the hashed password (you'll use this in your app)
+print(hashed_passwords[0])
